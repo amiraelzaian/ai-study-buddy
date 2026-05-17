cff077a5db9192af8484da1f95399c85dc2db900
Amira Alzaian <amiraelzaian204@gmail.com>
2026-05-17 21:36:18 +0200

    Add favicon and app icon images, and create web manifest
    - Added favicon image (favicon-16x16.png) for the application.
    - Added app icon image (icon.png) for the application.
    - Created a web manifest (site.webmanifest) to define application metadata and icons.

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,7 @@
 import Spinner from "@/app/_components/Spinner";
 function Loading() {
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center mt-40 mx-auto ">
       <Spinner />
     </div>
   );
