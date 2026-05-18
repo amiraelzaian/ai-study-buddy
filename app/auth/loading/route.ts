66fa88fee8cc63485da5e63cbc422a28e91cefd6
Amira Alzaian <amiraelzaian204@gmail.com>
2026-05-18 05:51:52 +0300

    fix: refactor authentication logic to handle token verification and session exchange

--- app/auth/loading/route.ts
+++ app/auth/loading/route.ts
@@ -2,18 +2,27 @@ import { createSupabaseServer } from "@/app/_lib/supabase/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  const code = new URL(request.url).searchParams.get("code");
-
-  if (!code) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type");
 
   const supabase = await createSupabaseServer();
-  const { error } = await supabase.auth.exchangeCodeForSession(code);
 
-  if (error) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (token_hash && type) {
+    const { error } = await supabase.auth.verifyOtp({
+      token_hash,
+      type: type as any,
+    });
+    if (!error)
+      return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  return NextResponse.redirect(new URL("/dashboard", request.url));
+  if (code) {
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error)
+      return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  return NextResponse.redirect(new URL("/login", request.url));
 }
